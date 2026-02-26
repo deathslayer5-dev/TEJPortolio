bc3c2425fc5c7850a8cb8d4ed760023b84258c6f
deathslayer5-dev <deathslayer532@gmail.com>
2026-02-26 16:13:51 -0500

    Added fantasy build, safety and user manual

---
+++ insert.py
@@ -0,0 +1,4 @@
+a = input("Enter the description of the activity\n")
+b = input("Enter the link to the img you have\n")
+c = input("Description of image\n")
+print(f"<div class=\"activity\">\n\t<div class=\"image\">\n\t\t<img src=\"{b}\" alt=\"{c}\" width=\"450\">\n\t</div>\n\t<div class =\"text\">\n\t\t<p>\n\t\t\t{a}\n\t\t</p>\n\t</div>\n</div>")
